641b33834e26e7ef90cb3722b14e6ef7328c2554
BarrHer <herve974.30@gmail.com>
2023-05-16 12:49:55 +0400

    Deploy website - based on e9902ed526ef0049b07450470b6f6382f2fecb6f

---
+++ assets/js/baddfc09.8dbb8816.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkbadgeuse=self.webpackChunkbadgeuse||[]).push([[1430],{5780:e=>{e.exports=JSON.parse('{"label":"hello","permalink":"/badgeuse/blog/tags/hello","allTagsPath":"/badgeuse/blog/tags","count":2}')}}]);
